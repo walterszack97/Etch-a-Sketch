e9b1258d811bb5d730eff56f10c0a9c9095426c8
zachary w <walterszack97@gmail.com>
2023-01-14 19:38:36 -0500

    Create input range slider to add cells to the grid and clear canvas when changed

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 const container = document.querySelector(".container");
 const divRow = document.createElement("div");
 const divColumn = document.createElement("div");
+const slider = document.querySelector("#gridSize");
+const value = document.querySelector("#value");
 
-function createDivs(){
-    for (let i = 0; i < 4; i++){
+function createDivs(size){
+    for (let i = 0; i < size; i++){
         const divColumn = document.createElement("div");
         divColumn.classList.add("divColumn");
         container.appendChild(divColumn);
-        for (let p = 0; p < 4; p++){
+
+        //remove divs when changing the slider value so that they dont add up indefinitly
+        slider.addEventListener('mousedown', (event) => {
+            document.querySelectorAll('.divColumn').forEach(e => e.remove());
+        })
+        for (let p = 0; p < size; p++){
             const divRow = document.createElement("div");
             divRow.classList.add("divRow");
             divColumn.appendChild(divRow);
             divRow.addEventListener("mouseover", (event) => {
                 divRow.style.backgroundColor = "red";
             })
+            slider.addEventListener('mousedown', (event) => {
+                document.querySelectorAll('.divRow').forEach(e => e.remove());
+            })
         }
     }
 }
 
+value.textContent = slider.value + 'x' + slider.value;
 
-createDivs();
+slider.addEventListener("mouseup", (event) => {
+    value.textContent = event.target.value + 'x' + event.target.value;
+    createDivs(slider.value);
+})
+
+createDivs(slider.value);
 
